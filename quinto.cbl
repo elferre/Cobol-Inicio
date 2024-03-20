ab165138a6adc092292fe490c73bd74c01ef21b3
Edgardo Gustavo <ed200520006@hotmail.com>
2024-03-20 11:55:04 -0300

    DECIMALES Y NEGATIVOS

---
+++ quinto.cbl
@@ -0,0 +1,23 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-QUINTO.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01 NUM1 PIC 99V99.
+       01 NUM2 PIC S99V99.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           MOVE 10.08 TO NUM1.
+            DISPLAY NUM1.
+            MOVE 12.08 TO NUM2.
+            DISPLAY NUM2.
+            MOVE -25.08 TO NUM2.
+            DISPLAY NUM2.
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-QUINTO.
