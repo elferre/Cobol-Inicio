3e64d2e630313c23df2c4859dba5c2978c3a246e
Edgardo Gustavo <ed200520006@hotmail.com>
2024-03-20 10:39:36 -0300

    Primer Display

--- primero.cbl
+++ primero.cbl
@@ -9,8 +9,15 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01  SALUDO PIC A(22).
+       01  TITULO PIC A(40) VALUE 'Tutorial Cobol'.
+       01  NUMERO PIC 9(1) VALUE 9.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
+            MOVE "BIENBENIDO" TO SALUDO.
+            DISPLAY "MUY BUENAS " SALUDO.
+            DISPLAY "TITULO : " TITULO.
+            DISPLAY "UN NUMERO : " NUMERO.
             STOP RUN.
        END PROGRAM Primero-NAME.
