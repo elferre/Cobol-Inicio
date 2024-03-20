fdcb3d3edff495777a906c3843f736617e311d50
Edgardo Gustavo <ed200520006@hotmail.com>
2024-03-20 14:06:29 -0300

    COPY subdividir CBL

---
+++ SUB-REGISTRO.cbl
@@ -0,0 +1,8 @@
+       FD EMPLEADOS-ARCHIVO.
+           01 EMPLEADOS-REGISTRO.
+               05 EMPLEADOS-ID PIC X(6).
+               05 EMPLEADOS-NOMBRE PIC X(25).
+               05 EMPLEADOS-APELLIDOS PIC X(35).
+               05 EMPLEADOS-EDAD PIC X(3).
+               05 EMPLEADOS-TELEFONO PIC X(9).
+               05 EMPLEADOS-DIRECCION PIC X(35).
