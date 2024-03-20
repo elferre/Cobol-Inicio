ec4598f31f0566a0c7e915a8ee7606c9f43cf70b
Edgardo Gustavo <ed200520006@hotmail.com>
2024-03-20 10:30:28 -0300

    vacio

---
+++ primero.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Edgardo
+      * Date: 20/3/24
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Primero-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM Primero-NAME.
